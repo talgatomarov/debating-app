3aa1e489231e1f1e55157eda8270a6605be8aa6f
talgatomarov <talgat.omarov.1999@gmail.com>
2020-09-01 22:29:20 +0600

    Add useStores hook

---
+++ src/hooks/useStores.ts
@@ -0,0 +1,7 @@
+import * as React from "react";
+import { storesContext } from "../contexts";
+import { AuthStore } from "../stores";
+
+export const useStores = (): { authStore: AuthStore } => {
+  return React.useContext(storesContext);
+};

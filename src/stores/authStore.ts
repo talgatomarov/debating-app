fac1c515d5dfb56a1ab89dc6eeb7a796d1d0d621
talgatomarov <talgat.omarov.1999@gmail.com>
2020-09-11 18:19:24 +0600

    Refactor interfaces from authStore

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,18 +1,6 @@
 import { observable, action } from "mobx";
 import firebase from "../firebase";
-
-interface User {
-  uid: string | undefined;
-  email: string | null | undefined;
-  emailVerified: boolean | undefined;
-  displayName: string | null | undefined;
-  photoURL: string | null | undefined;
-}
-
-interface AuthError {
-  code: string;
-  message: string;
-}
+import { User, AuthError } from "../interfaces";
 
 export class AuthStore {
   @observable user: User | null = null;

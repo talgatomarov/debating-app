101761c07fee591fb0294892a16a777559da9ff2
talgatomarov <talgat.omarov.1999@gmail.com>
2020-11-25 16:04:25 +0600

    Remove lobby routes test

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -19,7 +19,7 @@ const App: React.FC = () => {
           <Route path="/signin" component={SignIn} />
           <Route path="/reset-password" component={ResetPassword} />
           <LobbyRoutes path="/lobby" />
-          <Route exact path="/" component={Home} />
+          <Redirect exact from="/" to={"/lobby"} />
         </Switch>
       </Router>
     </>

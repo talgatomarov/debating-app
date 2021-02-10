1c6e4dbb29bd073772afb20879482c90c9404edb
talgatomarov <talgat.omarov.1999@gmail.com>
2021-02-10 13:21:13 +0600

    Add styles to DailyFrame

--- src/components/DailyFrame/DailyFrame.tsx
+++ src/components/DailyFrame/DailyFrame.tsx
@@ -1,12 +1,22 @@
 import React, { useRef, useEffect } from "react";
 import DailyIframe from "@daily-co/daily-js";
+import { makeStyles } from "@material-ui/core";
 
 interface DailyFrameProps {
   url: string;
 }
 
+const useStyles = makeStyles(() => ({
+  dailyiframe: {
+    width: "100%",
+    height: "100%",
+    border: "0",
+  },
+}));
+
 const DailyFrame: React.FC<DailyFrameProps> = ({ url }) => {
   const iframeRef = useRef<HTMLIFrameElement>(null);
+  const classes = useStyles();
 
   useEffect(() => {
     const daily = DailyIframe.wrap(iframeRef.current!);
@@ -16,6 +26,7 @@ const DailyFrame: React.FC<DailyFrameProps> = ({ url }) => {
   return (
     <iframe
       title="test"
+      className={classes.dailyiframe}
       ref={iframeRef}
       allow="camera; microphone; fullscreen"
     ></iframe>

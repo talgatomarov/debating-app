99066d832ac87174e5853169526f15cac5538fa2
talgatomarov <talgat.omarov.1999@gmail.com>
2021-02-16 20:54:36 +0600

    Implement test express endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,10 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as express from "express";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Test");
+});
+
+exports.daily = functions.https.onRequest(app);

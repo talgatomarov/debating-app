8a80ce2e4cef997f470814599a82a7370ac2e81e
talgatomarov <talgat.omarov.1999@gmail.com>
2021-02-16 21:43:05 +0600

    Add /rooms endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,20 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
+import axios from "axios";
 
 const app = express();
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Test");
+const dailyKey = functions.config().daily.key;
+
+router.get("/rooms", (req, res) => {
+  const url = "https://api.daily.co/v1/rooms";
+
+  return axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${dailyKey}`,
+    },
+  });
 });
 
 app.use("/daily", router);

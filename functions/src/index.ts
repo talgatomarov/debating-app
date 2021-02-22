9c0ebf34cee01665e86e83e632582c6b93e87bc4
talgatomarov <talgat.omarov.1999@gmail.com>
2021-02-22 19:33:12 +0600

    Use express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,20 @@
-import { getRooms, postRooms } from "./rooms";
+import * as express from "express";
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
 
-export { getRooms, postRooms };
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: "https://debating-app.firebaseio.com",
+});
+
+const app = express();
+
+app.get("/rooms", (req, res) => {
+  res.send("get room");
+});
+
+app.post("/rooms", (req, res) => {
+  res.send("post room");
+});
+
+exports.widgets = functions.https.onRequest(app);

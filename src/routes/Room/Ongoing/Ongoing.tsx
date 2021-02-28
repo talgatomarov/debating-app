320beb6789e07d5c0f117a23f1894b5b2e037685
talgatomarov <talgat.omarov.1999@gmail.com>
2021-02-28 14:47:28 +0600

    Add FailyFrame to Ongoing component

--- src/routes/Room/Ongoing/Ongoing.tsx
+++ src/routes/Room/Ongoing/Ongoing.tsx
@@ -1,7 +1,37 @@
+import { UserData } from "interfaces/UserData";
 import React from "react";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import app from "app";
+import { Alert } from "@material-ui/lab";
+import { Box, CircularProgress } from "@material-ui/core";
+import DailyFrame from "components/DailyFrame";
 
 const Ongoing: React.FC = () => {
-  return <div>Ongoing</div>;
+  const [userData, loading, error] = useDocumentData<UserData>(
+    app.firestore().collection("users").doc(app.auth().currentUser!.uid)
+  );
+
+  return (
+    <>
+      <div>Ongoing</div>
+      {error && (
+        <Alert severity="error" data-testid="error">
+          Room does not exists or could not fetch the data
+        </Alert>
+      )}
+      {loading && (
+        <Box display="flex" justifyContent="center" data-testid="loading">
+          <CircularProgress />
+        </Box>
+      )}
+      {userData?.meetingName && (
+        <DailyFrame
+          meetingName={userData.meetingName}
+          meetingToken={userData.meetingToken}
+        />
+      )}
+    </>
+  );
 };
 
 export default Ongoing;

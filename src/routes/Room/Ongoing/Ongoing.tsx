d3eeeaddc329e24d719e0ee5d97cafbd69d13491
talgatomarov <talgat.omarov.1999@gmail.com>
2021-03-07 17:05:52 +0600

    Refactor Ongoing

--- src/routes/Room/Ongoing/Ongoing.tsx
+++ src/routes/Room/Ongoing/Ongoing.tsx
@@ -1,33 +1,23 @@
-import { UserData } from "interfaces/UserData";
 import React from "react";
-import { useDocumentData } from "react-firebase-hooks/firestore";
-import app from "app";
 import { Alert } from "@material-ui/lab";
-import { Box, CircularProgress } from "@material-ui/core";
 import DailyFrame from "components/DailyFrame";
+import { useStores } from "hooks";
 
 const Ongoing: React.FC = () => {
-  const [userData, loading, error] = useDocumentData<UserData>(
-    app.firestore().collection("users").doc(app.auth().currentUser!.uid)
-  );
+  const { userStore } = useStores();
 
   return (
     <>
       <div>Ongoing</div>
-      {error && (
+      {!userStore.meetingName && (
         <Alert severity="error" data-testid="error">
           Room does not exists or could not fetch the data
         </Alert>
       )}
-      {loading && (
-        <Box display="flex" justifyContent="center" data-testid="loading">
-          <CircularProgress />
-        </Box>
-      )}
-      {userData?.meetingName && (
+      {userStore.meetingName && (
         <DailyFrame
-          meetingName={userData.meetingName}
-          meetingToken={userData.meetingToken}
+          meetingName={userStore.meetingName}
+          meetingToken={userStore.meetingToken}
         />
       )}
     </>

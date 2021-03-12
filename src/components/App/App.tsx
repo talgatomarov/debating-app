586fbec6e6c587c88e8c5b9ec6391eef6efafc3a
talgatomarov <talgat.omarov.1999@gmail.com>
2021-03-12 17:41:45 +0600

    Refacttor Router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,32 +1,15 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
-import { SignUp, SignIn, ResetPassword, Room, CreateRoom, Lobby } from "routes";
 import { CssBaseline } from "@material-ui/core";
 import "fontsource-roboto";
-import PrivateRoute from "components/PrivateRoute";
 import { StoresProvider } from "contexts";
+import Router from "./Router";
 
 const App: React.FC = () => {
   return (
     <>
       <StoresProvider>
         <CssBaseline />
-        <Router>
-          <Switch>
-            <Route path="/signup" component={SignUp} />
-            <Route path="/signin" component={SignIn} />
-            <Route path="/reset-password" component={ResetPassword} />
-            <PrivateRoute exact path="/room" component={Room} />
-            <PrivateRoute exact path="/create-room" component={CreateRoom} />
-            <PrivateRoute exact path="/lobby" component={Lobby} />
-            <Redirect exact from="/" to={"/lobby"} />
-          </Switch>
-        </Router>
+        <Router />
       </StoresProvider>
     </>
   );

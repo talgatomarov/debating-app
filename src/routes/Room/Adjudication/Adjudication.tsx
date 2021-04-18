75198e9175cf398e6c042054eeb10438eef9e2eb
talgatomarov <talgat.omarov.1999@gmail.com>
2021-04-18 18:12:52 +0600

    Add basic Adjudication component

--- src/routes/Room/Adjudication/Adjudication.tsx
+++ src/routes/Room/Adjudication/Adjudication.tsx
@@ -1,7 +1,21 @@
+import DailyFrame from "components/DailyFrame";
+import { useStores } from "hooks";
 import React from "react";
 
 const Adjudication: React.FC = () => {
-  return <div>Adjudication</div>;
+  const { userStore } = useStores();
+
+  return (
+    <>
+      <div>Adjudication</div>;
+      {userStore.meetingName && (
+        <DailyFrame
+          meetingName={userStore.meetingName}
+          meetingToken={userStore.meetingToken}
+        />
+      )}
+    </>
+  );
 };
 
 export default Adjudication;

6cb94f244a0bc628906072f8d08e8a7f0a5a1523
talgatomarov <talgat.omarov.1999@gmail.com>
2021-04-18 18:11:21 +0600

    Add "Start adjudicating" button

--- src/routes/Room/Deliberation/Deliberation.tsx
+++ src/routes/Room/Deliberation/Deliberation.tsx
@@ -1,13 +1,31 @@
+import { Button } from "@material-ui/core";
+import app from "app";
 import DailyFrame from "components/DailyFrame";
 import { useStores } from "hooks";
+import { Judge } from "interfaces/Judge";
 import React from "react";
+import { roomStore } from "stores";
 
 const Deliberation: React.FC = () => {
+  const currentUser = app.auth().currentUser!;
   const { userStore } = useStores();
 
   return (
     <>
       <div>Deliberation</div>
+      {roomStore.judges?.some(
+        (judge: Judge) => judge.uid === currentUser.uid
+      ) && (
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          onClick={() => roomStore.startAdjudication()}
+          style={{ margin: "1rem 0 1rem 0" }}
+        >
+          Start adjudication
+        </Button>
+      )}
       {userStore.meetingName && (
         <DailyFrame
           meetingName={userStore.meetingName}

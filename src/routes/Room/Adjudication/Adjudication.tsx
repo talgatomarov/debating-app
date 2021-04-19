00d6d73afd950d1d62af7138391ebf892546c69a
talgatomarov <talgat.omarov.1999@gmail.com>
2021-04-19 12:23:21 +0600

    Add end round button

--- src/routes/Room/Adjudication/Adjudication.tsx
+++ src/routes/Room/Adjudication/Adjudication.tsx
@@ -1,13 +1,30 @@
+import { Button } from "@material-ui/core";
+import app from "app";
 import DailyFrame from "components/DailyFrame";
 import { useStores } from "hooks";
+import { Judge } from "interfaces/Judge";
 import React from "react";
 
 const Adjudication: React.FC = () => {
-  const { userStore } = useStores();
+  const currentUser = app.auth().currentUser!;
+  const { userStore, roomStore } = useStores();
 
   return (
     <>
       <div>Adjudication</div>
+      {roomStore.judges?.some(
+        (judge: Judge) => judge.uid === currentUser.uid
+      ) && (
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          onClick={() => roomStore.startAdjudication()}
+          style={{ margin: "1rem 0 1rem 0" }}
+        >
+          End round
+        </Button>
+      )}
       {userStore.meetingName && (
         <DailyFrame
           meetingName={userStore.meetingName}
